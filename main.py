d9dbb036413d6dc26a6b09c318242e32646e926c
jonasstrube <26897240+jonasstrube@users.noreply.github.com>
2022-04-26 10:22:10 +0200

    restructure main file

--- main.py
+++ main.py
@@ -3,41 +3,6 @@ from copy import deepcopy
 from random import randint
 import __sudokumanager 
 
-def print_sudoku(sudoku: array):
-    for line in sudoku:
-        line_str: str = ''
-        for box in line:
-            if box[0]:
-                line_str += str(box[0]) + ' '
-            else:
-                line_str += '  '
-        print(line_str)
-
-def work_sudoku(sudoku: array) -> None:
-    for number in range(1, 10): # 1 to 9
-        for quadrant_index in range(9): # 9 quadrants, 0 to 8 
-            if not __sudokumanager.number_is_in_quadrant(number, quadrant_index, sudoku):
-                # fill in number if it only has one possible position 
-                possible_coordinates = __sudokumanager.get_possible_coordinates_of_number(number, quadrant_index, sudoku)
-                
-                if len(possible_coordinates) == 1:
-                    # TODO add sudokumanager-function to set number in sudoku / UGLY decentralized access of sudoku
-                    line = possible_coordinates[0][0]
-                    row = possible_coordinates[0][1]
-                    sudoku[line][row][0] = number
-                    __sudokumanager.erase_possible_numbers_at_position(line, row, sudoku)
-                    __sudokumanager.erase_possible_positions_of_number(number, quadrant_index, sudoku)
-                elif len(possible_coordinates) > 1:
-                    block_possible = __sudokumanager.coordinates_are_in_line(possible_coordinates)
-                    if block_possible:
-                        __sudokumanager.block_line_or_row(number, possible_coordinates, sudoku)
-                else:
-                    # Exception: there is no possible coordinate in the given quadrant for the number
-                    raise Exception
-            else:
-                # number is already in quadrant
-                pass
-
 def get_sudoku_input() -> array:
 
     prepared_sudokus: dict = {
@@ -106,6 +71,41 @@ def get_sudoku_input() -> array:
     else:
         print('that didnt work')
         return None
+
+def work_sudoku(sudoku: array) -> None:
+    for number in range(1, 10): # 1 to 9
+        for quadrant_index in range(9): # 9 quadrants, 0 to 8 
+            if not __sudokumanager.number_is_in_quadrant(number, quadrant_index, sudoku):
+                # fill in number if it only has one possible position 
+                possible_coordinates = __sudokumanager.get_possible_coordinates_of_number(number, quadrant_index, sudoku)
+                
+                if len(possible_coordinates) == 1:
+                    # TODO add sudokumanager-function to set number in sudoku / UGLY decentralized access of sudoku
+                    line = possible_coordinates[0][0]
+                    row = possible_coordinates[0][1]
+                    sudoku[line][row][0] = number
+                    __sudokumanager.erase_possible_numbers_at_position(line, row, sudoku)
+                    __sudokumanager.erase_possible_positions_of_number(number, quadrant_index, sudoku)
+                elif len(possible_coordinates) > 1:
+                    block_possible = __sudokumanager.coordinates_are_in_line(possible_coordinates)
+                    if block_possible:
+                        __sudokumanager.block_line_or_row(number, possible_coordinates, sudoku)
+                else:
+                    # Exception: there is no possible coordinate in the given quadrant for the number
+                    raise Exception
+            else:
+                # number is already in quadrant
+                pass
+
+def print_sudoku(sudoku: array):
+    for line in sudoku:
+        line_str: str = ''
+        for box in line:
+            if box[0]:
+                line_str += str(box[0]) + ' '
+            else:
+                line_str += '  '
+        print(line_str)
 
 def main():
 

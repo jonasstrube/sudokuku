bca39b0ec0dbddb831b1ae9ce3047ba54beb4e50
jonasstrube <26897240+jonasstrube@users.noreply.github.com>
2022-04-29 17:30:04 +0200

    add setup.py for setuptools and pypi

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="jonasstrube-sudokumanager",
+    version="0.1.1",
+    author="Jonas Strube",
+    description="A package for solving sudokus",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jonasstrube/sudokumanager",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.9",
+)

2bbce2a9a86a9a07c7c3be9ae4f23fbbb07c3de1
jonasstrube <26897240+jonasstrube@users.noreply.github.com>
2022-05-21 21:00:55 +0200

    fix versioning

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="sudokuku",
-    version="0.1.1",
+    version="0.1.0",
     author="Jonas Strube",
     description="A package for solving sudokus",
     long_description=long_description,

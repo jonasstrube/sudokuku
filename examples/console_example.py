c2e5ad94d96324b401de2065401347914937611c
jonasstrube <26897240+jonasstrube@users.noreply.github.com>
2022-07-05 16:07:10 +0200

    add harder sudoku for next steps

--- examples/console_example.py
+++ examples/console_example.py
@@ -45,6 +45,19 @@ def get_sudoku_input() -> array:
                 [None, 9, None, None, None, None, None, None, None],
                 [7, None, 1, None, 6, 9, None, None, 8]
             ]
+        ],
+        7: [
+            [
+                [None, 4, None, None, 1, None, None, 8, None],
+                [3, None, None, 4, None, 2, None, None, 6],
+                [None, None, None, 6, None, 8, None, None, None],
+                [None, 2, 3, None, None, None, 8, 5, None],
+                [8, None, None, None, None, None, None, None, 7],
+                [None, 5, 7, None, None, None, 3, 4, None],
+                [None, None, None, 5, None, 4, None, None, None],
+                [9, None, None, 2, None, 1, None, None, 5],
+                [None, 8, None, None, 7, None, None, 1, None]
+            ]
         ]
     }
 
